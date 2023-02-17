c0a352de4eddb87cdbb808f79d8218e14ed2d7ae
Lal Castro <lal.castro@icloud.com>
2023-02-17 12:11:21 +0530

    Enhancing the App

--- models/product.js
+++ models/product.js
@@ -1,35 +1,40 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = (onComplete) => {
-  fs.readFile(p, (error, fileContent) => {
-    if (error) {
-      onComplete([]);
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
     } else {
-      onComplete(JSON.parse(fileContent));
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
+
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (error) => {
-        console.log(error);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
-  static fetchAll(onComplete) {
-    getProductsFromFile(onComplete);
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
